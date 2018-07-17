c79de95f821978a10c946dcdba5d87e8cd942e6c
Marin <Marin.Marinov38@myhunter.cuny.edu>
2018-07-16 21:39:14 -0400

    "This time till the end.."

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,16 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Attribute routing
+            routes.MapMvcAttributeRoutes();
+
+            //Convention-based Routing
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"2017|2018}", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
