70e3c348ee060e635c5fde45c9c913e5238869ff
Marin <Marin.Marinov38@myhunter.cuny.edu>
2018-07-19 21:30:31 -0400

    "First exercise complete, doing fine so far.."

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,25 +4,62 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!" };
+            var movies = GetMovies();
 
-            return View(movie);
+            return View(movies);
         }
 
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
 
-        public ActionResult ByReleaseDate(int year, int month)
+
+        private IEnumerable<Movie> GetMovies()
         {
-            return Content(year + "/" + month);
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!"},
+                new Movie { Id = 2, Name = "Ip Man"}
+            };
         }
+
+
+
+        //EXAMPLE FROM LECTURES
+        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customer = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customer 2"}
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customer
+        //    };
+
+        //    //passing data to views VERY BAD (casting issues) DO NOT USE
+        //    //ViewData["Movie"] = movie;
+        //    //ViewBag.Movie = movie;
+
+        //    return View(viewModel);
+        //}
     }
 }
